d555e40abd99b061f3d73e54b0007217c683086e
Rafsan12 <rafsan.web.2022@gmail.com>
2025-05-01 08:00:58 +0600

    basic data type

--- module-1/module-1/src/1.4.ts
+++ module-1/module-1/src/1.4.ts
@@ -14,3 +14,16 @@ let middleName: null = null;
 
 // 5. undefined
 let address: undefined = undefined;
+
+// its anyType.its not redemanded to use it.
+let d;
+
+let f: number;
+
+// 6.Arrays
+let numbers: number[] = [1, 2, 3, 4, 5];
+let names: string[] = ["Rafsan", "Rafiq", "Rafi"];
+
+7; // tuple
+// you can use tuple to define a fixed length array with different types of data.
+let person: [string, number] = ["Rafsan", 30]; // [name, age]
