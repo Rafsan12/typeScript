1eb2631903b7642fb7e986a39b388ba11e34cd5c
Rafsan12 <rafsan.web.2022@gmail.com>
2025-05-01 07:38:20 +0600

    Start learing typeScript

---
+++ module-1/module-1/src/1.4.ts
@@ -0,0 +1,16 @@
+// Basis Data types
+
+// 1. string
+let firstName: string = "Rafsan";
+
+2; // number
+let age: number = 30;
+
+3; // boolean
+let isEmployed: boolean = true;
+
+// 4. null
+let middleName: null = null;
+
+// 5. undefined
+let address: undefined = undefined;
